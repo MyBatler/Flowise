0808e73b2929cbe0def81846e0833aca7d338fd6
MyBatler <e.david.adv@gmail.com>
2025-04-18 01:48:50 +0300

    Update App.jsx

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react'
+import LoginDialog from '@/ui-component/dialog/LoginDialog' // ודא שהנתיב נכון
+
 import { useSelector } from 'react-redux'
 
 import { ThemeProvider } from '@mui/material/styles'
@@ -15,18 +18,42 @@ import NavigationScroll from '@/layout/NavigationScroll'
 // ==============================|| APP ||============================== //
 
 const App = () => {
+const [isAuthenticated, setIsAuthenticated] = useState(() => {
+  return localStorage.getItem('loggedIn') === 'true'
+})
+
     const customization = useSelector((state) => state.customization)
 
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={themes(customization)}>
-                <CssBaseline />
-                <NavigationScroll>
-                    <Routes />
-                </NavigationScroll>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    )
+   return (
+  <>
+    {!isAuthenticated && (
+      <LoginDialog
+        show={true}
+        dialogProps={{ title: 'Login', confirmButtonName: 'Login' }}
+        onConfirm={(username, password) => {
+          if (username === 'admin' && password === '1234') {
+            localStorage.setItem('loggedIn', 'true')
+            setIsAuthenticated(true)
+          } else {
+            alert('Wrong credentials')
+          }
+        }}
+      />
+    )}
+
+    {isAuthenticated && (
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themes(customization)}>
+          <CssBaseline />
+          <NavigationScroll>
+            <Routes />
+          </NavigationScroll>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    )}
+  </>
+)
+
 }
 
 export default App
