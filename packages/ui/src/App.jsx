0de5b94d3503f884dd2deb50318f58de55bfd001
MyBatler <e.david.adv@gmail.com>
2025-04-18 18:30:32 +0300

    Update App.jsx

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState, useEffect } from 'react'
-import LoginDialog from '@/ui-component/dialog/LoginDialog' // ודא שהנתיב נכון
-
 import { useSelector } from 'react-redux'
 
 import { ThemeProvider } from '@mui/material/styles'
@@ -15,52 +12,21 @@ import themes from '@/themes'
 // project imports
 import NavigationScroll from '@/layout/NavigationScroll'
 
-const ENV_USERNAME = import.meta.env.VITE_FLOWISE_USERNAME
-const ENV_PASSWORD = import.meta.env.VITE_FLOWISE_PASSWORD
-
 // ==============================|| APP ||============================== //
 
 const App = () => {
-const [isAuthenticated, setIsAuthenticated] = useState(() => {
-  return localStorage.getItem('loggedIn') === 'true'
-})
-
     const customization = useSelector((state) => state.customization)
 
-   return (
-  <>
-    {!isAuthenticated && (
-      <LoginDialog
-        show={true}
-        dialogProps={{ title: 'Login', confirmButtonName: 'Login' }}
-onConfirm={(username, password) => {
-  console.log('USERNAME ENV:', ENV_USERNAME)
-  console.log('PASSWORD ENV:', ENV_PASSWORD)
-  console.log('USER INPUT:', username, password)
-
-  if (username === ENV_USERNAME && password === ENV_PASSWORD) {
-    localStorage.setItem('loggedIn', 'true')
-    setIsAuthenticated(true)
-  } else {
-    alert('Wrong credentials')
-  }
-}}
-      />
-    )}
-
-    {isAuthenticated && (
-      <StyledEngineProvider injectFirst>
-        <ThemeProvider theme={themes(customization)}>
-          <CssBaseline />
-          <NavigationScroll>
-            <Routes />
-          </NavigationScroll>
-        </ThemeProvider>
-      </StyledEngineProvider>
-    )}
-  </>
-)
-
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes(customization)}>
+                <CssBaseline />
+                <NavigationScroll>
+                    <Routes />
+                </NavigationScroll>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    )
 }
 
 export default App
